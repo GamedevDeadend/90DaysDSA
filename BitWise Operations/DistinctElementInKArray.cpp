c3f4b274dba79628d50a8c67da5f9e3df0951f4d
Tanmay1007 <tanmay.arawal@godspeedgames.com>
2023-06-17 16:12:23 +0530

    Distinct elements among pair of elements

---
+++ BitWise Operations/DistinctElementInKArray.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+// Find Distinct Element in a Array where other elements are repeating K times
+
+int main()
+{
+    int k = 0;
+    int size = 0;
+    int* a;
+    cout<<"Enter Value of K Amount of Repeatation of each value"<<endl;
+    cin>>k;
+
+    a = new int[2*k + 1];
+
+    cout<<"Enter The Elements"<<endl;
+    for(int i = 0; i<2*k + 1; i++)
+    {
+        cin>>a[i];
+    }
+
+    cout<<"Array is Loaded"<<endl;
+
+    int BitCountArray[32];
+
+    for(int i = 0; i<2*k + 1; i++)
+    {
+        while(a != 0)
+        {
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+   
+}
---
+++ BitWise Operations/TwoDistinctElements.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+//Two Distinct Elements in a array of paired numbers 
+int main()
+{
+    int a[] = {1,2,3,4,3,2,1,4,5,6};
+    int b = 0; 
+    int setbit_no = 0;
+
+   for( auto i : a)
+   {
+        b ^= i;
+   }
+
+   cout<<b<<endl;
+
+   setbit_no = b&~(b-1);
+   cout<<setbit_no<<endl;
+
+//    int* c = new int[sizeof(int)];
+//    int* d = new int [sizeof(int)];
+   int c = 0;
+   int d = 0;
+
+   for(int j = 0; j<10; j++)
+   {
+     if(a[j] & setbit_no)
+     {
+          cout<<"sdfhksdfkjhsdf"<<endl;
+          c = c^a[j]; 
+     }
+     else
+     {
+          d ^= d^a[j];
+     }
+   }
+
+ cout<<endl<<"Two Distinct Elements are-"<<endl<<c<<","<<d;
+}
